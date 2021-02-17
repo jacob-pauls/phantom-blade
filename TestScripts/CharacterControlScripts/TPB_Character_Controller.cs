aea12c66357e013d90424d6d74809fd7425fed48
Jacob Pauls <jakepauls99@gmail.com>
2021-02-16 22:54:54 -0800

    Additional controller refactoring

--- TestScripts/CharacterControlScripts/TPB_Character_Controller.cs
+++ TestScripts/CharacterControlScripts/TPB_Character_Controller.cs
@@ -4,34 +4,31 @@ using UnityEngine.Events;
 /**
  * Jake Pauls
  * TPB_Character_Controller
- * Controls character actions and movements
+ * Controls character actions, movements, and performs checks for various collision types
  */
 
 public class TPB_Character_Controller : MonoBehaviour
 {
-    [SerializeField] private float _speed = 1f;
-    [SerializeField] private float _jumpForce = 1f;
-    [SerializeField] private float _crouchResistance = 0.1f;
-
-    [SerializeField] private LayerMask _groundLayerMask;
-    
-    [SerializeField] private Collider2D _disabledColliderOnCrouch;
-
-    [SerializeField] private Transform _groundCheck;
-    [SerializeField] private Transform _ceilingCheck;
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private float jumpForce = 1f;
+    [SerializeField] private float crouchResistance = 0.1f;
+    [SerializeField] private LayerMask environmentLayer;
+    [SerializeField] private Collider2D disabledColliderOnCrouch;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform ceilingCheck;
 
     Rigidbody2D rb2D;
     BoxCollider2D bc2D;
     CircleCollider2D cc2D;
     SpriteRenderer spriteRenderer;
     
-    public UnityEvent _OnGroundEvent;
-    public UnityEvent _OnCrouchEvent;
+    public UnityEvent ON_GROUND_EVENT;
+    public UnityEvent ON_CROUCH_EVENT;
 
-    bool _isGrounded;
-    bool _isCrouching;
-    bool _canStandUp = true;
-    bool _isFacingRight = true;
+    bool isGrounded;
+    bool isCrouching;
+    bool canStandUp = true;
+    bool isFacingRight = true;
     
     void Awake() {
         rb2D = GetComponent<Rigidbody2D>();
@@ -40,10 +37,10 @@ public class TPB_Character_Controller : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
 
         // Initialize necessary events
-        if (_OnGroundEvent == null)        
-            _OnGroundEvent = new UnityEvent();
-        if (_OnCrouchEvent == null)
-            _OnCrouchEvent = new UnityEvent();
+        if (ON_GROUND_EVENT == null)        
+            ON_GROUND_EVENT = new UnityEvent();
+        if (ON_CROUCH_EVENT == null)
+            ON_CROUCH_EVENT = new UnityEvent();
     }
 
     void FixedUpdate() {
@@ -58,73 +55,72 @@ public class TPB_Character_Controller : MonoBehaviour
 
         // Check player input, apply velocity
         if (movement > 0) {
-            rb2D.velocity = new Vector2(_speed, rb2D.velocity.y);
+            rb2D.velocity = new Vector2(speed, rb2D.velocity.y);
         } else if (movement < 0) {
-            rb2D.velocity = new Vector2(-_speed, rb2D.velocity.y);
+            rb2D.velocity = new Vector2(-speed, rb2D.velocity.y);
         } else {
             rb2D.velocity = new Vector2(0f, rb2D.velocity.y);
         }
 
-        if (movement > 0 && !_isFacingRight) {
+        if (movement > 0 && !isFacingRight) {
             FlipCharacter();
-        } else if (movement < 0 && _isFacingRight) {
+        } else if (movement < 0 && isFacingRight) {
             FlipCharacter();
         }
     }
 
     void GroundCheck() {
         // Perform a linecast to the ground in reference to the "ground" layer mask
-        if (Physics2D.Linecast(transform.position, _groundCheck.position, _groundLayerMask)) {
-            _isGrounded = true;
-            _OnGroundEvent.Invoke();
+        if (Physics2D.Linecast(transform.position, groundCheck.position, environmentLayer)) {
+            isGrounded = true;
+            ON_GROUND_EVENT.Invoke();
         } else {
-            _isGrounded = false;
+            isGrounded = false;
         }
-        Debug.Log("_isGrounded Status -> " + _isGrounded);
     }
 
     void JumpCheck() {
-        if ((Input.GetKey("space")) && _isGrounded) {
-            rb2D.velocity = new Vector2(rb2D.velocity.x, _jumpForce);
+        if ((Input.GetKey("space")) && isGrounded) {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpForce);
         }
     }
 
     void CrouchCheck() {
-        if ((Input.GetKey("s") && _isGrounded) || !_canStandUp) {
-            rb2D.velocity = new Vector2(rb2D.velocity.x * _crouchResistance, 0f);
-            _isCrouching = true;
-            _OnCrouchEvent.Invoke();
+        if ((Input.GetKey("s") && isGrounded) || !canStandUp) {
+            rb2D.velocity = new Vector2(rb2D.velocity.x * crouchResistance, 0f);
+            isCrouching = true;
+            ON_CROUCH_EVENT.Invoke();
         } else {
             rb2D.velocity = new Vector2(rb2D.velocity.x, rb2D.velocity.y);
-            _isCrouching = false;
+            isCrouching = false;
         }
 
         DisableCrouchColliderCheck();
     }
 
     void DisableCrouchColliderCheck() {
-        RaycastHit2D ceilingRaycast = Physics2D.Raycast(_ceilingCheck.position, Vector2.up, 0.1f);
+        RaycastHit2D ceilingRaycast = Physics2D.Raycast(ceilingCheck.position, Vector2.up, 0.1f);
 
         // If we're not crouching, check if we can stand up
-        if (!_isCrouching) {
+        if (!isCrouching) {
             if (ceilingRaycast.collider != null) 
-                _canStandUp = false;
+                canStandUp = false;
         } else {
             if (ceilingRaycast.collider == null) 
-                _canStandUp = true;
+                canStandUp = true;
         }
 
         // Disable the top collider if we're crouching under an object
-        if (_isCrouching && _disabledColliderOnCrouch != null) {
-            _disabledColliderOnCrouch.enabled = false;
+        if (isCrouching && disabledColliderOnCrouch != null) {
+            disabledColliderOnCrouch.enabled = false;
         } else {
-            _disabledColliderOnCrouch.enabled = true;
+            disabledColliderOnCrouch.enabled = true;
         }
     }
 
     void FlipCharacter() {
         // Flip character direction and apply transform
-        _isFacingRight = !_isFacingRight;
+        isFacingRight = !isFacingRight;
         Vector3 flippedScale = this.transform.localScale;
         flippedScale.x *= -1;
         this.transform.localScale = flippedScale;
